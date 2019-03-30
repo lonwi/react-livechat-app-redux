a82b860b17943751c298c599594cbd1d4e5d9a5c
Wojciech Bubolka <bubolka@gmail.com>
2019-03-30 10:08:22 +0100

    dzialajaca wersja

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,52 +3,15 @@ import moment from 'moment';
 import * as io from 'socket.io-client';
 import './Chat.css';
 
-import { createStore } from 'redux'; // dostepne wszedzie
-import { connect, provider } from 'react-redux';  // dostepne w react
+// 7. Przepisuje wszystko co bylo oparte na this.state i this.handleAction na propsy
 
 class Chat extends Component {
-
     socketConnection = null;
 
-    constructor() {
-        super();
-        this.state = {
-            author: 'Wojtek B',
-            messages: [],
-            show: false
-        }
+    constructor(props) { // jezeli chcemy odzczytac propsy w construktor trzeba zdefiniowac
+        super(props);
         this.socketConnection = io.connect('https://socket-chat-server-yvhjqrpgyq.now.sh');
-
-        this.socketConnection.on('chat message', this.addMessage);
-    }
-
-    handleActions = (action) => {
-        switch (action.type) {
-            case 'ADD_MESSAGE':
-                this.setState({ messages: [...this.state.messages, action.payload] });
-                break;
-            case 'OPEN_CHAT':
-                this.setState({ show: true });
-                break;
-            case 'CLOSE_CHAT':
-                this.setState({ show: false });
-                break;
-
-            default: break;
-        }
-    }
-
-    addMessage = (message) => {
-        this.setState({ messages: [...this.state.messages, message] });
-        console.log(message);
-    }
-
-    handleOpen = () => {
-        this.setState({ show: true })
-    }
-
-    handleClose = () => {
-        this.setState({ show: false })
+        this.socketConnection.on('chat message', props.addMessage);
     }
 
     handleSubmit = (e) => {
@@ -60,11 +23,9 @@ class Chat extends Component {
         if (this.message.value) {
             const message = {
                 text: this.message.value,
-                authorId: this.state.author,
-                //timestamp: moment().unix()
+                authorId: this.props.author,
             }
             this.socketConnection.emit('chat message', message);
-            //this.setState({ messages: [...this.state.messages, message] });
             this.message.value = '';
         }
     }
@@ -80,16 +41,15 @@ class Chat extends Component {
     render() {
         return (
             <div>
-                {/* <section className={`Chat ${this.state.show ? `Chat--show` : `Chat--hide`}`}> */}
-                <section className={`Chat animated ${this.state.show ? `Chat--show fadeIn` : `Chat--hide fadeOut faster`}`}>
+                <section className={`Chat animated ${this.props.show ? `Chat--show fadeIn` : `Chat--hide fadeOut faster`}`}>
                     <header className="Chat-header">
                         <h1>Live Chat</h1>
-                        <button onClick={this.handleClose}>&times;</button>
+                        <button onClick={this.props.closeChat}>&times;</button>
                     </header>
 
                     <div className="Chat-main">
                         <div className="Chat-messages">
-                            {this.state.messages.map((message) => <div className="Chat-message" key={message.id}>{message.text} <div className="Chat-message--author">{message.authorId}, {this.timeAgo(message.timestamp)}</div></div>)}
+                            {this.props.messages.map((message) => <div className="Chat-message" key={message.id}>{message.text} <div className="Chat-message--author">{message.authorId}, {this.timeAgo(message.timestamp)}</div></div>)}
                         </div>
                     </div>
 
@@ -105,11 +65,13 @@ class Chat extends Component {
                     </footer>
                 </section>
 
-                {/* <button className={`Chat-fab ${!this.state.show ? `Chat-fab--show` : `Chat-fab--hide`}`} onClick={this.handleOpen}>+</button> */}
-                <button className={`Chat-fab animated ${!this.state.show ? `Chat-fab--show fadeIn` : `Chat-fab--hide fadeOut faster`}`} onClick={this.handleOpen}>+</button>
+                <button className={`Chat-fab animated ${!this.props.show ? `Chat-fab--show fadeIn` : `Chat-fab--hide fadeOut faster`}`} onClick={this.props.openChat}>+</button>
             </div>
         )
     }
 }
+
+
+// Podpinamy store do komponentu
 
 export default Chat;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,67 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Chat from './components/Chat';
+// 1. Import niezbednych elementow
+import { createStore } from 'redux'; // dostepne wszedzie
+import { connect, Provider } from 'react-redux';  // dostepne w react
 
-ReactDOM.render(<Chat />, document.getElementById('root'));
+// 2. Tworzymy Reducer
+
+// przyjmuje zawsze 2 parametry state i action
+// Zawsze wraca nowy stan
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_MESSAGE':
+            return { ...state, messages: [...state.messages, action.payload] }
+        case 'OPEN_CHAT':
+            return { ...state, show: true }
+        case 'CLOSE_CHAT':
+            return { ...state, show: false }
+        default: return state;
+    }
+}
+// 3. Domyslny Stan Aplikacji
+const defaultState = {
+    author: 'Wojtek B',
+    messages: [],
+    show: false,
+    bla: 'bla'
+}
+
+// 4. Tworzymy nasz store
+const store = createStore(
+    reducer,
+    defaultState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+
+// 5. Wybieramy wlasciwosci ze stanu zeby podpiac je do komponentu
+const mapStateToProps = state => {
+    return {
+        author: state.author,
+        messages: state.messages,
+        show: state.show
+    }
+}
+
+
+// 6. Wybieramy akcje z ktorych bedzie chcial korzystac komponent
+const mapDispatchToProps = dispatch => {
+    return {
+        openChat: () => dispatch({ type: 'OPEN_CHAT' }),
+        closeChat: () => dispatch({ type: 'CLOSE_CHAT' }),
+        addMessage: (message) => dispatch({ type: 'ADD_MESSAGE', payload: message }),
+    }
+}
+
+const ConnectedApp = connect(mapStateToProps, mapDispatchToProps)(Chat);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedApp />
+    </Provider>,
+    document.getElementById('root')
+);
+// ReactDOM.render(<Chat />, document.getElementById('root'));
